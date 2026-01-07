ca5c2bff1cfd0061b60eecf9fb84918cdd33b7b5
zhuyuesen <907980361@qq.com>
2026-01-07 18:52:02 +0800

    初始化笔记

---
+++ 03_变量.py
@@ -0,0 +1,3 @@
+a = 1
+a = False
+print('a', a) 
---
+++ test.py
@@ -0,0 +1,13 @@
+print('100 + 200 = ', 100 + 200)
+
+a = 100;
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+
+
+
+
+print(r'''hello,\n
+    world''')
